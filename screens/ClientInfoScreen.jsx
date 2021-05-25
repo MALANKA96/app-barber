a7a4b52d0f884908084909110d3470dad217a36e
MALANKA96 <voksa.kolya@gmail.com>
2021-05-26 01:43:35 +0300

    init react navigation

---
+++ screens/ClientInfoScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+const ClientInfoScreen = () => {
+  return <TextInfo>Client Information Screen</TextInfo>;
+};
+
+const TextInfo = styled.Text`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export default ClientInfoScreen;
